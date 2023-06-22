f97e8e38c8f88b1aaf9c600711b91b8255a2c852
Javad Jafari <javad2147@yahoo.com>
2023-06-22 20:43:18 +0330

    #28 added theme colors and the typography and fonts

--- app/src/main/java/ir/thatsmejavad/backgroundable/ui/theme/Theme.kt
+++ app/src/main/java/ir/thatsmejavad/backgroundable/ui/theme/Theme.kt
@@ -10,31 +10,74 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val darkColorScheme = darkColorScheme(
+    primary = Color(0xFFFFB59F),
+    onPrimary = Color(0xFF5F1600),
+    primaryContainer = Color(0xFF862300),
+    onPrimaryContainer = Color(0xFFFFDBD1),
+    secondary = Color(0xFFA1C9FF),
+    onSecondary = Color(0xFF00325A),
+    secondaryContainer = Color(0xFF004880),
+    onSecondaryContainer = Color(0xFFD2E4FF),
+    tertiary = Color(0xFFD8C58D),
+    onTertiary = Color(0xFF3A2F05),
+    tertiaryContainer = Color(0xFF524619),
+    onTertiaryContainer = Color(0xFFF5E1A7),
+    error = Color(0xFFFFB4AB),
+    onError = Color(0xFF690005),
+    errorContainer = Color(0xFF93000A),
+    onErrorContainer = Color(0xFFFFDAD6),
+    background = Color(0xFF191C1D),
+    onBackground = Color(0xFFE1E3E3),
+    inverseOnSurface = Color(0xFF191C1D),
+    inverseSurface = Color(0xFFE1E3E3),
+    inversePrimary = Color(0xFFAF3000),
+    outlineVariant = Color(0xFF53433F),
+    surface = Color(0xFF101415),
+    onSurface = Color(0xFFC4C7C7),
+    surfaceVariant = Color(0xFF53433F),
+    onSurfaceVariant = Color(0xFF53433F),
+    surfaceTint = Color(0xFFD8C2BC),
+    outline = Color(0xFFA08C87),
+    scrim = Color(0xFF000000),
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
+private val lightColorScheme = lightColorScheme(
+    primary = Color(0xFFAF3000),
+    onPrimary = Color(0xFFFFFFFF),
+    primaryContainer = Color(0xFFFFDBD1),
+    onPrimaryContainer = Color(0xFF3A0A00),
+    secondary = Color(0xFF0F61A4),
+    onSecondary = Color(0xFFFFFFFF),
+    secondaryContainer = Color(0xFFD2E4FF),
+    onSecondaryContainer = Color(0xFF001C37),
+    tertiary = Color(0xFF6B5D2F),
+    onTertiary = Color(0xFFFFFFFF),
+    tertiaryContainer = Color(0xFFF5E1A7),
+    onTertiaryContainer = Color(0xff231B00),
+    error = Color(0xffBA1A1A),
+    onError = Color(0xFFFFFFFF),
+    errorContainer = Color(0xffFFDAD6),
+    onErrorContainer = Color(0xFF410002),
+    background = Color(0xFFFAFDFD),
+    onBackground = Color(0xFF191C1D),
+    inverseOnSurface = Color(0xFFEFF1F1),
+    inverseSurface = Color(0xFF2E3132),
+    inversePrimary = Color(0xFFFFB59F),
+    outlineVariant = Color(0xFFD8C2BC),
+    surface = Color(0xFFF8FAFA),
+    onSurface = Color(0xFF191C1D),
+    surfaceVariant = Color(0xFFF5DED7),
+    onSurfaceVariant = Color(0xFF53433F),
+    surfaceTint = Color(0xFFAF3000),
+    outline = Color(0xFF85736E),
+    scrim = Color(0xFF000000),
 )
 
 @Composable
@@ -49,8 +92,8 @@ fun BackgroundableTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkColorScheme
+        else -> lightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
--- app/src/main/java/ir/thatsmejavad/backgroundable/ui/theme/Type.kt
+++ app/src/main/java/ir/thatsmejavad/backgroundable/ui/theme/Type.kt
@@ -2,33 +2,98 @@ package ir.thatsmejavad.backgroundable.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ir.thatsmejavad.backgroundable.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+
+val Poppins = FontFamily(
+    Font(
+        resId = R.font.poppins_medium,
+        weight = FontWeight.Medium,
+    ),
+    Font(
+        resId = R.font.poppins_regular,
+        weight = FontWeight.Normal,
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+)
+
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 57.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-     */
+        fontSize = 11.sp
+    ),
 )
