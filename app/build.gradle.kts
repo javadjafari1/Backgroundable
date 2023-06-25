c375001f24c0ab239f4179d04a64d1acf8e74eef
Javad Jafari <javad2147@yahoo.com>
2023-06-25 22:40:57 +0330

    update versionCode and VersionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "ir.thatsmejavad.backgroundable"
         minSdk = 24
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
