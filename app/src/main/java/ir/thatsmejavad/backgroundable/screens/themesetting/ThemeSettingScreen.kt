c616656e96c9b1ef463923c8a8ecd00454b7288b
Javad Jafari <javad2147@yahoo.com>
2023-06-27 23:09:14 +0330

    #67 add change theme button

--- app/src/main/java/ir/thatsmejavad/backgroundable/screens/themesetting/ThemeSettingScreen.kt
+++ app/src/main/java/ir/thatsmejavad/backgroundable/screens/themesetting/ThemeSettingScreen.kt
@@ -1,29 +1,46 @@
 package ir.thatsmejavad.backgroundable.screens.themesetting
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import ir.thatsmejavad.backgroundable.R
+import ir.thatsmejavad.backgroundable.core.sealeds.Theme
 import ir.thatsmejavad.backgroundable.model.UserPreferences
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,6 +51,8 @@ fun ThemeSettingScreen(
     val userPreferences by viewModel.userPreferencesFlow.collectAsStateWithLifecycle(
         initialValue = UserPreferences()
     )
+    val scope = rememberCoroutineScope()
+
     Scaffold(
         topBar = {
             LargeTopAppBar(
@@ -51,7 +70,70 @@ fun ThemeSettingScreen(
             )
         }
     ) { paddingValues ->
-        Column(Modifier.padding(paddingValues)) {
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .verticalScroll(rememberScrollState()),
+        ) {
+
+            val tabs = listOf(
+                Theme.LightTheme,
+                Theme.DarkTheme,
+                Theme.FollowSystem
+            )
+            val selectedTabIndex by remember(userPreferences.theme) {
+                mutableIntStateOf(
+                    tabs.indexOf(
+                        userPreferences.theme
+                    )
+                )
+            }
+
+            TabRow(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .clip(MaterialTheme.shapes.extraLarge),
+                selectedTabIndex = selectedTabIndex,
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                indicator = { tabPositions ->
+                    Box(
+                        modifier = Modifier
+                            .tabIndicatorOffset(tabPositions[selectedTabIndex])
+                            .fillMaxHeight()
+                            .clip(MaterialTheme.shapes.extraLarge)
+                            .padding(10.dp)
+                            .background(
+                                color = MaterialTheme.colorScheme.primary,
+                                shape = MaterialTheme.shapes.extraLarge
+                            )
+                            .zIndex(-1f)
+                    )
+                },
+                tabs = {
+                    tabs.forEachIndexed { index, theme ->
+                        Tab(
+                            modifier = Modifier.padding(vertical = 8.dp),
+                            selected = selectedTabIndex == index,
+                            selectedContentColor = MaterialTheme.colorScheme.onPrimary,
+                            unselectedContentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                            onClick = { scope.launch { viewModel.updateTheme(theme) } },
+                            text = {
+                                Text(
+                                    text = stringResource(
+                                        when (theme) {
+                                            Theme.DarkTheme -> R.string.label_dark
+                                            Theme.FollowSystem -> R.string.label_follow_system
+                                            Theme.LightTheme -> R.string.label_light
+                                        }
+                                    )
+                                )
+                            }
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(46.dp))
 
             MaterialYouRow(
                 isChecked = userPreferences.isMaterialYouEnabled,
