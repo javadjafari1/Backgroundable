e13d70f61708ab07863b7ef94697c3b4181e0d8c
Javad Jafari <javad2147@yahoo.com>
2023-07-15 22:48:58 +0330

    #73 fix the HexagonShape in the CollectionListScreen

---
+++ app/src/main/java/ir/thatsmejavad/backgroundable/common/ui/HexagonShape.kt
@@ -0,0 +1,45 @@
+package ir.thatsmejavad.backgroundable.common.ui
+
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import java.lang.Float.min
+import kotlin.math.sqrt
+
+object HexagonShape : Shape {
+
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        return Outline.Generic(
+            path = drawCustomHexagonPath(size)
+        )
+    }
+}
+
+fun drawCustomHexagonPath(size: Size): Path {
+    return Path().apply {
+        val radius = min(size.width / 2f, size.height / 2f)
+        customHexagon(radius, size)
+    }
+}
+
+private fun Path.customHexagon(radius: Float, size: Size) {
+    val triangleHeight = (sqrt(3.0) * radius / 2)
+    val centerX = size.width / 2
+    val centerY = size.height / 2
+
+    moveTo(centerX, centerY + radius)
+    lineTo((centerX - triangleHeight).toFloat(), centerY + radius / 2)
+    lineTo((centerX - triangleHeight).toFloat(), centerY - radius / 2)
+    lineTo(centerX, centerY - radius)
+    lineTo((centerX + triangleHeight).toFloat(), centerY - radius / 2)
+    lineTo((centerX + triangleHeight).toFloat(), centerY + radius / 2)
+
+    close()
+}
