70e4c93c790cb6c4956727f5d09a13fa904d54bd
Javad Jafari <javad2147@yahoo.com>
2023-08-11 23:38:06 +0330

    #21 add drawableRes Annotation to Contributor data class

--- app/src/main/java/ir/thatsmejavad/backgroundable/model/Contributor.kt
+++ app/src/main/java/ir/thatsmejavad/backgroundable/model/Contributor.kt
@@ -1,14 +1,16 @@
 package ir.thatsmejavad.backgroundable.model
 
+import androidx.annotation.DrawableRes
+
 data class Contributor(
     val name: String,
     val position: String,
-    val image: Int,
+    @DrawableRes val image: Int,
     val links: List<ContributorLink>
 )
 
 data class ContributorLink(
-    val icon: Int,
+    @DrawableRes val icon: Int,
     val url: String,
     val name: String
 )
