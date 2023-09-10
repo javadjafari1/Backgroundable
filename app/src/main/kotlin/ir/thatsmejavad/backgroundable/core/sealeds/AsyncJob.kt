4aff6903f8c035b93749102ff551448b4de9d472
Javad Jafari <javad2147@yahoo.com>
2023-09-10 23:25:34 +0330

    use sealed interface instead of class fro the AsyncJob

--- app/src/main/kotlin/ir/thatsmejavad/backgroundable/core/sealeds/AsyncJob.kt
+++ app/src/main/kotlin/ir/thatsmejavad/backgroundable/core/sealeds/AsyncJob.kt
@@ -1,8 +1,8 @@
 package ir.thatsmejavad.backgroundable.core.sealeds
 
-sealed class AsyncJob<out T>(private val value: T?) {
-    data class Success<T>(val value: T) : AsyncJob<T>(value)
-    data class Fail<T>(val exception: Exception) : AsyncJob<T>(null)
-    data object Loading : AsyncJob<Nothing>(null)
-    data object Uninitialized : AsyncJob<Nothing>(null)
+sealed interface AsyncJob<out T> {
+    data class Success<T>(val value: T) : AsyncJob<T>
+    data class Fail<T>(val exception: Exception) : AsyncJob<T>
+    data object Loading : AsyncJob<Nothing>
+    data object Uninitialized : AsyncJob<Nothing>
 }
