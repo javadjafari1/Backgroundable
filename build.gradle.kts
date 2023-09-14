014640aa6d0c5399609a649ce3d08290dab21a8c
Javad Jafari <javad2147@yahoo.com>
2023-09-14 14:58:08 +0330

    remove project wide build.gradle.kts( because this project is single module)

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlin) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.protobuf) apply false
-    alias(libs.plugins.kotlinSerialization)
-    alias(libs.plugins.detekt)
-}
