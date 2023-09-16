d20b7de51b212124bd8c42c015b3f9d078ed5c17
Javad Jafari <javad2147@yahoo.com>
2023-09-16 23:22:29 +0330

    #93 remove insets when the Media is Zoom Mode

--- app/src/main/kotlin/ir/thatsmejavad/backgroundable/screens/mediadetail/MediaDetailScreen.kt
+++ app/src/main/kotlin/ir/thatsmejavad/backgroundable/screens/mediadetail/MediaDetailScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -35,6 +36,7 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.OutlinedIconButton
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -127,6 +129,11 @@ fun MediaDetailScreen(
     }
 
     BackgroundableScaffold(
+        contentWindowInsets = if (isToolsVisible) {
+            ScaffoldDefaults.contentWindowInsets
+        } else {
+            WindowInsets(0, 0, 0, 0)
+        },
         topBar = {
             AnimatedVisibility(
                 visible = isToolsVisible && title.isNotEmpty(),
