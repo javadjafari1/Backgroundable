47e56dcaa3b320aa17e1f3803e29273db1f06667
Javad Jafari <javad2147@yahoo.com>
2023-10-05 21:39:51 +0330

    turn decodeEnumsCaseInsensitive on in Json

--- app/src/main/kotlin/ir/thatsmejavad/backgroundable/di/modules/NetworkModule.kt
+++ app/src/main/kotlin/ir/thatsmejavad/backgroundable/di/modules/NetworkModule.kt
@@ -26,6 +26,7 @@ class NetworkModule {
     @Provides
     fun provideJson(): Json {
         return Json {
+            decodeEnumsCaseInsensitive = true
             ignoreUnknownKeys = true
             coerceInputValues = true
             explicitNulls = false
