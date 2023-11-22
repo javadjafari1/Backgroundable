7feb14a8cdce2c5141af8adaecd2301bdd3c91d3
Javad Jafari <javad2147@yahoo.com>
2023-11-22 22:36:40 +0330

    update versionCode and versionNumber, fix crash of CollectionListViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "ir.thatsmejavad.backgroundable"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.2.2"
+        versionCode = 6
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/kotlin/ir/thatsmejavad/backgroundable/screens/collectionlist/CollectionListViewModel.kt
+++ app/src/main/kotlin/ir/thatsmejavad/backgroundable/screens/collectionlist/CollectionListViewModel.kt
@@ -3,6 +3,7 @@ package ir.thatsmejavad.backgroundable.screens.collectionlist
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import ir.thatsmejavad.backgroundable.core.SnackbarManager
 import ir.thatsmejavad.backgroundable.data.datastore.ColumnCountsPreferences
 import ir.thatsmejavad.backgroundable.data.db.entity.CollectionEntity
@@ -25,6 +26,7 @@ class CollectionListViewModel @Inject constructor(
         private set
 
     val collection: StateFlow<PagingData<CollectionEntity>> = collectionRepository.getCollections()
+        .cachedIn(viewModelScope)
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(),
