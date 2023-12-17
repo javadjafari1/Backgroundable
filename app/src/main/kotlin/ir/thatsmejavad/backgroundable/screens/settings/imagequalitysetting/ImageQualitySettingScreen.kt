314565993c850941c1eab70440d150afaa0e8338
Javad Jafari <javad2147@yahoo.com>
2023-12-17 17:36:47 +0330

    #27 fix bugs on the persian lang

--- app/src/main/kotlin/ir/thatsmejavad/backgroundable/screens/settings/imagequalitysetting/ImageQualitySettingScreen.kt
+++ app/src/main/kotlin/ir/thatsmejavad/backgroundable/screens/settings/imagequalitysetting/ImageQualitySettingScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -37,6 +38,7 @@ import androidx.navigation.NavController
 import ir.thatsmejavad.backgroundable.R
 import ir.thatsmejavad.backgroundable.common.ui.BackgroundableScaffold
 import ir.thatsmejavad.backgroundable.core.sealeds.ImageQuality
+import ir.thatsmejavad.backgroundable.core.sealeds.ImageQuality.Companion.toResId
 import ir.thatsmejavad.backgroundable.core.viewmodel.daggerViewModel
 
 @Composable
@@ -82,6 +84,7 @@ private fun ImageQualitySettingScreen(
     ) { paddingValues ->
         Column(
             modifier = Modifier
+                .fillMaxSize()
                 .padding(paddingValues)
                 .verticalScroll(rememberScrollState())
                 .padding(16.dp),
@@ -112,7 +115,7 @@ private fun ImageQualitySettingScreen(
                         selected = imageQuality == quality,
                         onClick = { changeQuality(quality) }
                     ) {
-                        Text(text = quality.toString())
+                        Text(text = stringResource(quality.toResId()))
                     }
                 }
             }
@@ -135,73 +138,86 @@ private fun ImageQualitySettingScreen(
 
                 Spacer(modifier = Modifier.size(4.dp))
 
-                val text = buildAnnotatedString {
-                    append(stringResource(R.string.label_low_desc))
-                    addStyle(
-                        SpanStyle(
-                            color = MaterialTheme.colorScheme.onSurface,
-                        ),
-                        start = 0,
-                        end = 4
-                    )
+                val lowText = buildAnnotatedString {
+                    val low = "${stringResource(R.string.label_low)}: "
+                    val lowDesc = stringResource(R.string.label_low_desc)
+                    append(low)
+                    append(lowDesc)
+
                     addStyle(
                         SpanStyle(
                             color = MaterialTheme.colorScheme.surfaceTint,
                         ),
-                        start = 4,
-                        end = 46
-                    )
-                    appendLine()
-                    append(stringResource(R.string.label_medium_desc))
-                    addStyle(
-                        SpanStyle(
-                            color = MaterialTheme.colorScheme.onSurface,
-                        ),
-                        start = 49,
-                        end = 56
-                    )
-                    addStyle(
-                        SpanStyle(
-                            color = MaterialTheme.colorScheme.surfaceTint,
-                        ),
-                        start = 56,
-                        end = 95
-                    )
-                    appendLine()
-                    append(stringResource(R.string.label_high_desc))
-                    addStyle(
-                        SpanStyle(
-                            color = MaterialTheme.colorScheme.onSurface,
-                        ),
-                        start = 96,
-                        end = 101
-                    )
-                    addStyle(
-                        SpanStyle(
-                            color = MaterialTheme.colorScheme.surfaceTint,
-                        ),
-                        start = 101,
-                        end = 138
-                    )
-                    appendLine()
-                    append(stringResource(R.string.label_ultra_desc))
-                    addStyle(
-                        SpanStyle(
-                            color = MaterialTheme.colorScheme.onSurface,
-                        ),
-                        start = 139,
-                        end = 145
-                    )
-                    addStyle(
-                        SpanStyle(
-                            color = MaterialTheme.colorScheme.surfaceTint,
-                        ),
-                        start = 146,
-                        end = 175
+                        start = low.length,
+                        end = low.length + lowDesc.length
                     )
                 }
+
                 Text(
-                    text = text,
+                    text = lowText,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+
+                val mediumText = buildAnnotatedString {
+                    val medium = "${stringResource(R.string.label_medium)}: "
+                    val mediumDesc = stringResource(R.string.label_medium_desc)
+                    append(medium)
+                    append(mediumDesc)
+
+                    addStyle(
+                        SpanStyle(
+                            color = MaterialTheme.colorScheme.surfaceTint,
+                        ),
+                        start = medium.length,
+                        end = medium.length + mediumDesc.length
+                    )
+                }
+
+                Text(
+                    text = mediumText,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+
+                val highText = buildAnnotatedString {
+                    val high = "${stringResource(R.string.label_high)}: "
+                    val highDesc = stringResource(R.string.label_high_desc)
+                    append(high)
+                    append(highDesc)
+
+                    addStyle(
+                        SpanStyle(
+                            color = MaterialTheme.colorScheme.surfaceTint,
+                        ),
+                        start = high.length,
+                        end = high.length + highDesc.length
+                    )
+                }
+
+                Text(
+                    text = highText,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+                val ultraText = buildAnnotatedString {
+                    val ultra = "${stringResource(R.string.label_ultra)}: "
+                    val ultraDesc = stringResource(R.string.label_ultra_desc)
+                    append(ultra)
+                    append(ultraDesc)
+
+                    addStyle(
+                        SpanStyle(
+                            color = MaterialTheme.colorScheme.surfaceTint,
+                        ),
+                        start = ultra.length,
+                        end = ultra.length + ultraDesc.length
+                    )
+                }
+
+                Text(
+                    text = ultraText,
+                    color = MaterialTheme.colorScheme.onSurface,
                     style = MaterialTheme.typography.titleMedium,
                 )
             }
