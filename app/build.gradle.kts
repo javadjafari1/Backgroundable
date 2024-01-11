92e86d81a8f7150a4c9865fd159588a86127e19a
Javad Jafari <javad2147@yahoo.com>
2024-01-12 00:48:16 +0330

    update the version to 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "ir.thatsmejavad.backgroundable"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.3.1"
+        versionCode = 9
+        versionName = "1.3.2"
 
         resourceConfigurations.addAll(listOf("en", "fa"))
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
