1c82d4f5eb4cfcfabaafe71cfc7c0d74a3f2fc19
javadjafari <javad2147@yahoo.com>
2024-07-15 12:08:00 +0330

    update version name and number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "ir.thatsmejavad.backgroundable"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.3.3"
+        versionCode = 11
+        versionName = "1.3.4"
 
         resourceConfigurations.addAll(listOf("en", "fa"))
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
