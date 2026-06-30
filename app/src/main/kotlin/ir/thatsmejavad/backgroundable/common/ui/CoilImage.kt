f610ca0d81e39e2ee101f9b6ea3993ca5523f7eb
javadjafari <javad2147@yahoo.com>
2026-06-30 23:00:43 +0330

    migrate telephoto to coil 3

--- app/src/main/kotlin/ir/thatsmejavad/backgroundable/common/ui/CoilImage.kt
+++ app/src/main/kotlin/ir/thatsmejavad/backgroundable/common/ui/CoilImage.kt
@@ -8,12 +8,13 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import coil.request.ImageRequest
+import androidx.compose.ui.platform.LocalResources
+import coil3.asDrawable
 import coil3.compose.AsyncImage as AsyncImage3
 import coil3.request.ImageRequest as ImageRequest3
 import coil3.request.crossfade as crossfade3
 import ir.thatsmejavad.backgroundable.BuildConfig
-import me.saket.telephoto.zoomable.coil.ZoomableAsyncImage
+import me.saket.telephoto.zoomable.coil3.ZoomableAsyncImage
 
 @Composable
 fun CoilImage(
@@ -45,12 +46,15 @@ fun ZoomableCoilImage(
     onClick: ((Offset) -> Unit)? = null,
     onDrawableLoaded: (Drawable) -> Unit = {},
 ) {
+    val context = LocalContext.current
+    val resource = LocalResources.current
+
     ZoomableAsyncImage(
         modifier = modifier,
-        model = ImageRequest.Builder(LocalContext.current)
+        model = ImageRequest3.Builder(context)
             .placeholderMemoryCacheKey(BuildConfig.IMAGE_SERVER_URL + placeHolder)
             .data(BuildConfig.IMAGE_SERVER_URL + url)
-            .crossfade(2_000)
+            .crossfade3(2_000)
             .listener(
                 onError = { _, _ ->
                     isLoading(false)
@@ -59,7 +63,7 @@ fun ZoomableCoilImage(
                     isLoading(true)
                 },
                 onSuccess = { _, result ->
-                    onDrawableLoaded(result.drawable)
+                    onDrawableLoaded(result.image.asDrawable(resource))
                     isLoading(false)
                 }
             )
